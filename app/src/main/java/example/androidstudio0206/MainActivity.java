f44889550948a0167b74a73b2d5694a4628eff97
hongzhirui <1162996795@qq.com>
2019-06-22 20:51:51 +0800

    Initial commit

---
+++ app/src/main/java/example/androidstudio0206/MainActivity.java
@@ -0,0 +1,19 @@
+package example.androidstudio0206;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ImageView imageView = (ImageView)findViewById(R.id.id_image);
+//        Drawable image = getDrawable(R.drawable.im);
+//        imageView.setImageDrawable(image);
+    }
+}
